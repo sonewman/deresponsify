fb165cd42fbcb0dac6aa0ff9fb008567040c33b5
Sam Newman <acutecoder@gmail.com>
2013-05-09 14:35:52 +0100

    added regexps for matchin media queries, added basic test need more

---
+++ test/main.js
@@ -0,0 +1,9 @@
+var chai = require('chai')
+  , expect = chai.expect
+;
+
+describe('Main', function () {
+  it('Main File', function () {
+    expect(true).to.be.true;
+  });
+});
---
+++ test/splitter.js
@@ -0,0 +1,16 @@
+var chai = require('chai')
+  , expect = chai.expect
+  , splitter = require('../lib/splitter')
+;
+ 
+describe('Splitter Object', function () {
+  it('Object should be defined', function () {
+    expect(splitter).to.not.be.undefined;
+   });
+
+//  describe('#splitBlocks()', function () {
+
+//  });
+
+
+});
